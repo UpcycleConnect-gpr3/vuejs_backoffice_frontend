e62ed4b94fd81b315d0989e27a0fd23096a5e81d
0jikuji0 <the.jikuji_dev@proton.me>
2026-04-15 10:04:20 +0200

    feat(router): update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { TabBordPage } from '@/pages'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: TabBordPage,
+    },
+  ],
 })
 
 export default router
