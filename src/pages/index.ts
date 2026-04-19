670acf0dc1d48275e2f365b64007a0f94498b676
0jikuji0 <the.jikuji_dev@proton.me>
2026-04-19 11:21:18 +0200

    feat(categories): add CategoriesPage with Phosphor icons

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as DashboardPage } from './DashboardPage.vue'
 export { default as UsersPage } from './UsersPage.vue'
 export { default as PrestatairePage } from './PrestatairePage.vue'
+export { default as CategoriesPage } from './CategoriesPage.vue'
